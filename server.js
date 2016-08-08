6136a2e52705aa521952175a7a9693bdc63a77f4
Simon <simon@jampot.design>
2016-08-08 00:06:18 +0000

    REPLACE-ME

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+exports.app = app;
+exports.runServer = runServer;
